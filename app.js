6495cc1afd2b6a3ea821efe639a64abce15a1a27
ben hassene <yasserbenhassene@mac-mini-de-ben.home>
2024-05-17 01:20:22 +0200

    Color Ramdom

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 let btn = document.querySelector ('#cc')
 let page = document.querySelector ("body")
+
 btn.addEventListener ('click' , changeColor)
 function changeColor(){
-    page.style.backgroundColor = "#" + Math.random().toString(16).slice(2,8)
+    let x = "#" + Math.random().toString(16).slice(2,8)
+    page.style.backgroundColor = x
     document.body.style.transition = "all 0.9s "
+    btn.style.color = x
     
 }
